74f784ea6d9827f6363fef2fee4ed6e55536b3c9
KesovStavros <112382419+KesovStavros@users.noreply.github.com>
2022-09-17 12:29:30 +0300

    Create send.php

---
+++ send.php
@@ -0,0 +1,12 @@
+<?php
+
+if (mail("kesalov0@gmail.com",
+    "Новая программа должна пройти модерацию" ,
+    "Текст:" .$txt."/n". 
+    "фаил:".$apk."/n"., 
+    "from:kesovstavros0@gmail.com /r/n"
+    
+    )
+
+) 
+?>
