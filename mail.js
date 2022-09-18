efe9a5b3f41300cf034b5d832f8796db3e508235
KesovStavros <112382419+KesovStavros@users.noreply.github.com>
2022-09-18 11:00:24 +0300

    Create mail.js

---
+++ mail.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBmGXPXiSs98zbFU_H5qWR621KjsTr7z-Y",
+
+    authDomain: "kaccounts-1e9fd.firebaseapp.com",
+
+    databaseURL: "https://kaccounts-1e9fd-default-rtdb.firebaseio.com",
+
+    projectId: "kaccounts-1e9fd",
+
+    storageBucket: "kaccounts-1e9fd.appspot.com",
+
+    messagingSenderId: "983353111158",
+
+    appId: "1:983353111158:web:bc2e34aab3f06e219c2557"
+
+  }; 
+
+firebase.initializeApp(firebaseConfig);
