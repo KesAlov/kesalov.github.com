4da22ae00e1035a287b69508f18a4573e9583b07
KesovStavros <112382419+KesovStavros@users.noreply.github.com>
2022-09-20 19:45:22 +0300

    Create mail.js

---
+++ mail.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+  $("#form").submit(function() {
+
+    $.ajax({
+
+      type: "POST",
+
+      url: "mail.php",
+
+      data: $(this).serialize()
+
+    }).done(function() {
+
+      $(this).find("input").val("");
+
+      alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+
+      $("#form").trigger("reset");
+
+    });
+
+    return false;
+
+  });
+
+  
+
+});
