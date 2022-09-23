45001f362fc66f35f26e8649c7c0f91f378f1627
KesovStavros <112382419+KesovStavros@users.noreply.github.com>
2022-09-23 22:10:34 +0300

    Create sample.php

---
+++ sample.php
@@ -0,0 +1,7 @@
+user_pref("capability.policy.policynames", "allowclipboard");
+
+user_pref("capability.policy.allowclipboard.sites", "https://www.mozilla.org");
+
+user_pref("capability.policy.allowclipboard.Clipboard.cutcopy", "allAccess");
+
+user_pref("capability.policy.allowclipboard.Clipboard.paste", "allAccess");
