f215800710598b37d0b2504140a2366a205e9e8c
Stavros Kesov <112382419+KesovStavros@users.noreply.github.com>
2022-10-23 21:03:26 +0300

    update users.js

--- social/js/users.js
+++ social/js/users.js
@@ -1,6 +1,7 @@
 
 function login() {
-	if ( uid == '0000-0000-0000-0000') {
+	var uid = document.getElementById('uid');
+if ( uid == '0000-0000-0000-0000') {
 		setCookie("username", "testaccount");
 		}
 		
