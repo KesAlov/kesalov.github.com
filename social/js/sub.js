4225e33d3b1f82b244c64b8197d6d10b026ed50e
Stavros Kesov <112382419+KesovStavros@users.noreply.github.com>
2022-10-26 18:29:28 +0300

    update sub.js

---
+++ social/js/sub.js
@@ -0,0 +1,20 @@
+var requestURL = 'https://kesalov.github.io/social/databases/json/subs.json';
+var request = new XMLHttpRequest();
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+  var subs = request.response;
+ } 
+ 
+ 
+
+function sub(profile) {
+	
+	var myJSON = { "name": profile};
+myJSON
+var myString = JSON.stringify(myJSON);
+myString
+request.open('GET', requestURL);
+request.responseType = 'text'; // now we're getting a string!
+request.send();
+} 
