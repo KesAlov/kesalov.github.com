4778aa2227090ebf84519d20fd482adf13b5a960
Stavros Kesov <112382419+KesovStavros@users.noreply.github.com>
2022-10-30 16:47:19 +0300

    update generator.js

---
+++ kasandbox/generator.js
@@ -0,0 +1,5 @@
+function generator(blocks) {
+	document.write( blocks );
+	}
+	
+	generator("<span style='backround-color: green;'>00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000</span><br>");
