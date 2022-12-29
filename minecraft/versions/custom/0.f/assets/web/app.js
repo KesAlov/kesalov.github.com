42f6937c3b15681af7c2127db0a0b1c374b22811
Stavros Kesov <112382419+KesovStavros@users.noreply.github.com>
2022-12-29 17:09:50 +0300

    update app.js

--- minecraft/versions/custom/0.f/assets/web/app.js
+++ minecraft/versions/custom/0.f/assets/web/app.js
@@ -1,5 +1,7 @@
 function submitcode() {
 var code = document.getElementById("code").value;
-if("code"="4d6f6e6f6368726f6d65"){
-  window.location.href = "https://kesalov.github.io/";
+if(code == "4d6f6e6f6368726f6d65"){
+	var code2 = config('введите код №2');
+	
  } 
+}
