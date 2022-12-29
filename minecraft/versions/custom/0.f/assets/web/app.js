1afb0b7e9df6af136516f244217cd8249ef9328e
Stavros Kesov <112382419+KesovStavros@users.noreply.github.com>
2022-12-29 20:33:26 +0300

    update app.js

--- minecraft/versions/custom/0.f/assets/web/app.js
+++ minecraft/versions/custom/0.f/assets/web/app.js
@@ -5,4 +5,5 @@ if(code == "4d6f6e6f6368726f6d65"){
 	if(code2 == "6129326662388890") {
 		alert("#000000-#ffffff.jar/assets/secret/secret/don't open/no/no/no/no/secret.zip(passworld: 666)/don't open.zip(passworld: 888)/no.zip(passworld: hex of word minecraft)/youblackifopen.zip(passworld: hex of word black)/");
  } 
+ 
 }
