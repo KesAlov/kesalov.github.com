2829129ac275a90119e0b5d0b3eb33f8c9c999e8
Stavros Kesov <112382419+KesovStavros@users.noreply.github.com>
2023-01-14 12:05:40 +0300

    update search.js

--- search.js
+++ search.js
@@ -1,4 +1,4 @@
 function search() {
 			var searchcontent = document.getElementById('search').value;
-			document.write('<input type="search" id="search" Placeholder="поиск"><button onclick="search();" >найти</button>' + '<iframe src="' + searchcontent + '" width="100%" height="100%"></iframe>' );
+			document.write('<link rel="stylesheet" href="https://kesalov.github.io/search.css"><script src="https://kesalov.github.io/search.js"></script><input type="search" id="search" Placeholder="поиск"><button onclick="search();" >найти</button>' + '<iframe src="' + searchcontent + '" width="100%" height="100%"></iframe>' );
 } 
